68f3d72c5181e1ecb889764b46bf1c10c6660db7
Jaye <jaye1248276@gmail.com>
2024-10-20 11:07:09 +0800

    perf: 优化代码逻辑

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,22 +1,21 @@
 import type { Action } from "wxt/browser";
 
 export default defineBackground(() => {
-  browser.storage.sync.get('color').then(res => res.color && (color = res.color ));
   const size = 128
+  const canvas = new OffscreenCanvas(size, size);
+  const ctx = canvas.getContext("2d")!;
   let color = '#d84942'
   let oldTime = ''
 
-  function createIcon() {
+  function createIcon(force?: boolean) {
     const now = new Date();
     const minutes = (now.getMinutes() + '').padStart(2, '0')
-    if (oldTime === minutes) return
+    if (!force && oldTime === minutes) return
     
     const hours = (now.getHours() + '').padStart(2, '0')
     oldTime = minutes
 
-    const canvas = new OffscreenCanvas(size, size);
-    const ctx = canvas.getContext("2d")!;
-
+    ctx.clearRect(0, 0, size, size);
     ctx.font = "bold 8em monospace";
     ctx.fillStyle = color;
     ctx.fillText(hours, size / 2 * 0.5, 60);
@@ -25,32 +24,32 @@ export default defineBackground(() => {
     return ctx.getImageData(0, 0, size, size) as Action.ImageDataType
   }
 
-  function updateIcon() {
-    try {
-      const svgIcon = createIcon();
-      if (!svgIcon) return
+  function updateIcon(svgIcon = createIcon()) {
+    if (!svgIcon) return
 
-      browser.action.setIcon({ imageData: svgIcon }).catch(error => {
-        console.error('设置图标时出错:', error);
-      });
-    } catch (error) {
-      console.error('生成图标 URL 时出错:', error);
-    }
+    browser.action.setIcon({ imageData: svgIcon }).catch(error => {
+      console.error('设置图标时出错:', error);
+    });
   }
 
   browser.runtime.onMessage.addListener((message) => {
     if (message === 'updateIcon') {
       browser.storage.sync.get('color').then(res => {
         color = res.color;
-        updateIcon()
+        updateIcon(createIcon(true))
       });
     }
   })
 
-  browser.runtime.onInstalled.addListener(() => {
-    updateIcon()
-    setInterval(updateIcon, 3e3)
-  })
+  browser.management.getSelf((extensionInfo) => {
+    if (extensionInfo.enabled) {
+      browser.storage.sync.get('color').then(res => {
+        res.color && (color = res.color )
+        updateIcon(createIcon(true))
+        setInterval(updateIcon, 3e3)
+      })
+    }
+  });
 
   browser.alarms.create("updateIcon", { periodInMinutes: 0.5 }); 
   browser.alarms.onAlarm.addListener((alarm) => {
